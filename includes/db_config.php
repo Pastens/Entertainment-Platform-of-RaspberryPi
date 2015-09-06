7b81ea52667808f67678913b17f9a52c25ed3b53
刘昊程 <liuhc@staff.zjutv.com>
2015-09-06 10:58:46 +0800

    test scripts update

--- includes/db_config.php
+++ includes/db_config.php
@@ -2,7 +2,7 @@
 	global $conn;
 	$mysql_database = "localhost";
 	$mysql_username = "root";
-	$mysql_password = "lhc1112@lhc";
+	$mysql_password = "password";
 	$mysql_dbname   = "Entertainment";
 	$conn = new mysqli($mysql_database,$mysql_username,$mysql_password,$mysql_dbname);
 ?>
